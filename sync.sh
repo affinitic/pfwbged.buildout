6b2baeb79f1cd85f05ca36dee0b00c491ee64e1d
Vincent Fretin <vincent.fretin@gmail.com>
2013-03-17 12:06:33 +0100

    Added script to sync entrouvert and collective repositories

---
+++ sync.sh
@@ -0,0 +1,9 @@
+cd src
+for pkg in collective.contact.core collective.contact.widget collective.dms.basecontent collective.dms.batchimport collective.dms.mailcontent collective.dms.thesaurus; do
+
+    cd $pkg
+    git remote add entrouvert git+ssh://git@repos.entrouvert.org/$pkg.git
+    git pull; git pull entrouvert master; git push; git push entrouvert master
+    cd -
+
+done
