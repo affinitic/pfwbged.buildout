ad700c46f59f14d96a036b596017b8041854ff2f
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-19 17:11:16 +0100

    Added collective.task to sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 cd src
-for pkg in collective.contact.core collective.contact.widget collective.dms.basecontent collective.dms.batchimport collective.dms.mailcontent collective.dms.thesaurus; do
+for pkg in collective.contact.core collective.contact.widget collective.dms.basecontent collective.dms.batchimport collective.dms.mailcontent collective.dms.thesaurus collective.task; do
 
     cd $pkg
     git remote add entrouvert git+ssh://git@repos.entrouvert.org/$pkg.git
